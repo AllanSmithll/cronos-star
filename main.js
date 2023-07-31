a468ce781bd9547213d4c49c43e5a9f22a75d698
allansmithll <allanammancio2004@gmail.com>
2023-07-31 19:02:27 -0300

    Versão que mostra a hora atual

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 function displayHours() {
     const cronosDiv = document.getElementById("cronos");
     cronosDiv.innerHTML = "";
-    document.getElementById("cronos").innerText = hours;
+
+    const hour = new Date().getHours().toString().padStart(2,"0");
+    const minute = new Date().getMinutes().toString().padStart(2,"0");
+    const second = new Date().getSeconds().toString().padStart(2,"0");
+    document.getElementById("cronos").innerText = hour + ":" + minute + ":" + second;
 
     for (let i = 1; i <= 12; i++) {
         const hourMarker = document.createElement("div");
@@ -13,4 +17,4 @@ function displayHours() {
 }
 
 displayHours();
-setInterval(displayHours, 1000)
+setInterval(displayHours, 1000);
