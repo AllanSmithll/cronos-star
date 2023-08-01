40f34890f228ad24ab7e90d563936e54edee8910
allansmithll <allanammancio2004@gmail.com>
2023-08-01 00:59:06 -0300

    Versão final

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# cronos-star
-Relógio feito com HTML, CSS e JavaScript
+# Cronos Star
+
+## Descrição
+
+Cronos Star é um aplicativo de relógio desenvolvido em HTML, CSS e JavaScript. O aplicativo exibe a hora atual com precisão de milissegundos e possui um design responsivo que se adapta a diferentes tamanhos de tela.
+
+![Ícone do site](./img/clock-logo.png)
+
+## Funcionalidades
+
+- Exibição da hora atual com precisão de milissegundos.
+- Design responsivo para telas pequenas, médias e grandes.
+
+## Pré-requisitos
+
+Para executar o Cronos Star, você precisará de um navegador da web moderno que suporte HTML, CSS e JavaScript.
+
+## Instalação
+
+1. Clone este repositório em sua máquina local ou faça o download dos arquivos em formato ZIP.
+
+2. Navegue até o diretório do projeto.
+
+3. Abra o arquivo `index.html` em seu navegador da web ou clique no link aqui mesmo no README.md: https://allansmithll.github.io/cronos-star/.
+
+## Licença
+
+Cronos Star é licenciado sob a Licença Apache 2.0. Consulte o site [Apache Software Foundation](https://www.apache.org/licenses/LICENSE-2.0.html) para obter mais informações sobre os termos da licença.
+
+## Créditos
+
+- Ícone do site feito por [Sergei Kokota
+](https://www.facebook.com/Iconko-1093341900698327/)
+- Fonte "Poppins" por Google Fonts: [Poppins](https://fonts.google.com/specimen/Poppins)
+
+## Contato
+
+Se tiver alguma dúvida ou sugestão, sinta-se à vontade para entrar em contato:
+
+- Nome: Allan Amancio
+- E-mail: contato.allanamancio@gmail.com
--- main.js
+++ main.js
@@ -9,7 +9,8 @@ function displayTime() {
     const hours = now.getHours().toString().padStart(2, "0");
     const minutes = now.getMinutes().toString().padStart(2, "0");
     const seconds = now.getSeconds().toString().padStart(2, "0");
-    cronosDiv.innerText = `${hours}:${minutes}:${seconds}`;
+    const miliseconds = now.getMilliseconds().toString().padStart(3, "0");
+    cronosDiv.innerText = `${hours}:${minutes}:${seconds}:${miliseconds}`;
 }
 
 function displayHours() {
@@ -22,7 +23,8 @@ function displayHours() {
     const hour = new Date().getHours().toString().padStart(2,"0");
     const minute = new Date().getMinutes().toString().padStart(2,"0");
     const second = new Date().getSeconds().toString().padStart(2,"0");
-    document.getElementById("cronos").innerText = hour + ":" + minute + ":" + second;
+    const miliseconds = new Date().getMilliseconds().toString().padStart(3, "0");
+    document.getElementById("cronos").innerText = hour + ":" + minute + ":" + second + ":" + miliseconds;
 
     for (let i = 1; i <= 12; i++) {
         const hourMarker = document.createElement("div");
@@ -31,53 +33,12 @@ function displayHours() {
         hourMarker.style.transform = `rotate(${angle}deg)`;
         cronosDiv.appendChild(hourMarker);
     }
-    document.getElementById("btn-stopwatch").style.display = "inline-block";
-    document.getElementById("btn-pause").style.display = "none";
-    document.getElementById("btn-continue").style.display = "none";
     document.getElementById("btn-clock").style.display = "inline-block";
-
-    if (cronometerActivate == true && cronometerActivate == false) {
-        stopwatchInterval = setInterval(displayHours, 1000);
-    }
-}
-
-function resetClock() {
-    const cronosDiv = document.getElementById("cronos");
-    cronosDiv.innerText = "00:00:00";
-    cronometerActivate = true;
-    clockActivate = false;
-    clearInterval(clockInterval);
-    clearInterval(stopwatchInterval);
-
-    document.getElementById("btn-stopwatch").style.display = "inline-block";
-    document.getElementById("btn-pause").style.display = "inline-block";
-    document.getElementById("btn-continue").style.display = "inline-block";
-    document.getElementById("btn-clock").style.display = "inline-block";
-}
-
-function pauseStopwatch() {
-    clearInterval(stopwatchInterval);
-    document.getElementById("btn-pause").style.display = "none";
-    document.getElementById("btn-continue").style.display = "inline-block";
-}
-
-function continueStopwatch() {
-    stopwatchInterval = setInterval(displayHours, 1000);
-    document.getElementById("btn-pause").style.display = "inline-block";
-    document.getElementById("btn-continue").style.display = "none";
+    // Atualiza o relógio a cada 1 milissegundo
+    clockInterval = setInterval(displayTime, 1);
+    document.querySelector("h2").style.display = "block";
 }
 
 document.getElementById("btn-clock").addEventListener("click", () => {
     displayHours();
-    if (clockActivate == true) {
-        clearInterval(stopwatchInterval);
-        clockInterval = setInterval(displayTime, 1000);
-    }
-    document.getElementById("btn-pause").style.display = "none";
-    document.getElementById("btn-continue").style.display = "none";
 });
-
-document.getElementById("btn-stopwatch").addEventListener("click", () => {
-    resetClock();
-});
-
