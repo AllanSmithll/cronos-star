f4c9dd10f204704e51ee61823329a1fab049acb7
allansmithll <allanammancio2004@gmail.com>
2023-08-01 11:52:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function displayTime() {
     const minutes = now.getMinutes().toString().padStart(2, "0");
     const seconds = now.getSeconds().toString().padStart(2, "0");
     const miliseconds = now.getMilliseconds().toString().padStart(3, "0");
-    cronosDiv.innerText = `${hours}:${minutes}:${seconds}:${miliseconds}`;
+    cronosDiv.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 function displayHours() {
@@ -23,8 +23,7 @@ function displayHours() {
     const hour = new Date().getHours().toString().padStart(2,"0");
     const minute = new Date().getMinutes().toString().padStart(2,"0");
     const second = new Date().getSeconds().toString().padStart(2,"0");
-    const miliseconds = new Date().getMilliseconds().toString().padStart(3, "0");
-    document.getElementById("cronos").innerText = hour + ":" + minute + ":" + second + ":" + miliseconds;
+    document.getElementById("cronos").innerText = hour + ":" + minute + ":" + second;
 
     for (let i = 1; i <= 12; i++) {
         const hourMarker = document.createElement("div");
@@ -34,8 +33,7 @@ function displayHours() {
         cronosDiv.appendChild(hourMarker);
     }
     document.getElementById("btn-clock").style.display = "inline-block";
-    // Atualiza o relógio a cada 1 milissegundo
-    clockInterval = setInterval(displayTime, 1);
+    clockInterval = setInterval(displayTime, 1000);
     document.querySelector("h2").style.display = "block";
 }
 
